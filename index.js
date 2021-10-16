fec93c71a310ba0824500c57b9a65efb0190d691
Brandon Lepore <bjt.lepore@gmail.com>
2021-10-16 13:11:28 +1030

    parameter change - 'target_id' accessible as 'options.target' property

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ class SpineDropdown {
     constructor(stylesheet) {
         this.ss = document.querySelector(`link[href*=${stylesheet}]`)
         if (this.ss) {
-            this.made = false
             this.ss = this.ss.sheet
             const rules = [
                 '.s-dd{position:relative}',
@@ -17,19 +16,16 @@ class SpineDropdown {
             for (const r of rules) { this.ss.insertRule(r) }
         } else { throw new ReferenceError(`${stylesheet} is not a referenced stylesheet in document`) }
     }
-    make(list, target_id, options) {
-        const el = document.getElementById(target_id)
-        if (!el) { throw new ReferenceError(`${target_id} is not a referenced id in document`) }
+    make(list, options) {
         const li = list
-        let _sel
+        let el = null
+        let _sel = ''
         let _rep = false
         let _cls = ''
         let _clsB = ''
 
         function isValidInit() {
-            function required(param) {
-                if (param) { return true } else { throw new Error(`${param} parameter must be defined`) }
-            }
+            function required(param) { if (param) { return true } else { throw new Error(`${param} parameter must be defined`) } }
 
             function throwTypeError(e, t) { throw new TypeError(`${e} is not ${t}`) }
 
@@ -43,24 +39,33 @@ class SpineDropdown {
 
             if (required(li)) { if (isArray(li)) { _sel = li[0] } else { throwTypeError(li, 'an array') } }
             if (options) {
-                if (options && isObject(options)) {
-                    if (options.hasOwnProperty('selected')) {
-                        const o_s = options.selected
+                const o = options
+                if (o && isObject(o)) {
+                    if (o.hasOwnProperty('target')) {
+                        const o_t = o.target
+                        if (isString(o_t)) {
+                            el = document.getElementById(o_t)
+                            if (!el) { throw new ReferenceError(`${o_t} is not a referenced id in document`) } else {
+                                if (o.hasOwnProperty('replace')) {
+                                    const o_r = o.replace
+                                    if (isBoolean(o_r)) { _rep = o_r } else { throwTypeError(o_r, 'boolean') }
+                                }
+                            }
+                        } else { throwTypeError(o_t, 'a string') }
+                    }
+                    if (o.hasOwnProperty('selected')) {
+                        const o_s = o.selected
                         if (isString(o_s)) { _sel = o_s } else { throwTypeError(o_s, 'a string') }
                     }
-                    if (options.hasOwnProperty('replace')) {
-                        const o_r = options.replace
-                        if (isBoolean(o_r)) { _rep = o_r } else { throwTypeError(o_r, 'boolean') }
-                    }
-                    if (options.hasOwnProperty('class')) {
-                        const o_c = options.class
+                    if (o.hasOwnProperty('class')) {
+                        const o_c = o.class
                         if (isString(o_c)) { _cls = o_c } else { throwTypeError(o_c, 'a string') }
                     }
-                    if (options.hasOwnProperty('classBody')) {
-                        const o_cb = options.classBody
+                    if (o.hasOwnProperty('classBody')) {
+                        const o_cb = o.classBody
                         if (isString(o_cb)) { _clsB = o_cb } else { throwTypeError(o_cb, 'a string') }
                     }
-                } else { throwTypeError(options, 'an object') }
+                } else { throwTypeError(o, 'an object') }
             }
             return true
         }
@@ -78,17 +83,16 @@ class SpineDropdown {
             }
             d.append(dl)
             f.append(d)
-            if (_rep) { el.replaceWith(f) } else { el.appendChild(f) }
-            this.made = true
+            if (el) { if (_rep) { el.replaceWith(f) } else { el.appendChild(f) } } else { return f }
         }
     }
     init() {
-        if (this.made) {
-            const CSS_HEAD = this.CSS_HEAD
-            const CSS_LIST = this.CSS_LIST
-            const CSS_OPEN = this.CSS_OPEN
-            return new Promise(function(resolve, reject) {
-                const dropdowns = document.getElementsByClassName(CSS_HEAD)
+        const CSS_HEAD = this.CSS_HEAD
+        const CSS_LIST = this.CSS_LIST
+        const CSS_OPEN = this.CSS_OPEN
+        return new Promise(function(resolve, reject) {
+            const dropdowns = document.getElementsByClassName(CSS_HEAD)
+            if (dropdowns.length > 0) {
                 try {
                     for (const dropdown of dropdowns) {
                         const el_list = dropdown.getElementsByClassName(CSS_LIST)[0]
@@ -97,11 +101,14 @@ class SpineDropdown {
                             dropdown.addEventListener('click', function() { el_list.classList.toggle(CSS_OPEN) })
                             for (const option of options) { option.addEventListener('click', function() { dropdown.dataset.selected = this.textContent }) }
                             resolve(true)
-                        } else { console.log(`Warning: missing '${CSS_LIST}' class from dropdown`) }
+                        } else { console.log(`Warning: missing spine-dropdown list`) }
                     }
                 } catch (e) { resolve(null) }
-            })
-        } else { throw new Error(`at least one dropdown must be made (use 'classInstance.make(<Array>, <String>, <Object>)')`) }
+            } else {
+                resolve(null)
+                console.log(`Warning: missing spine-dropdown/s`)
+            }
+        })
     }
 }
 module.exports = { SpineDropdown }
